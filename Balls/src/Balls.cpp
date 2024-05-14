0b92849b40af2b72d2563cf7d2b814000d6b01b9
siofra-mc <seanathon.mccarthy@gmail.com>
2024-05-13 20:56:43 -0700

    premake fix part2

--- Balls/src/Balls.cpp
+++ Balls/src/Balls.cpp
@@ -1,158 +1,16 @@
-// Taken from https://learnopengl.com/code_viewer_gh.php?code=src/1.getting_started/1.2.hello_window_clear/hello_window_clear.cpp and modified to work with glfw
-// All credit go to Joey De Vries: https://joeydevries.com/#home author of Learn OpenGL.
-// Modified by: Jakob Törmä Ruhl
-/* Functionality extended by Síofra McCarthy */
-
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include <GL\glew.h>
-#include <unordered_map>
-#include <GLFW/glfw3.h>
-#include <glm.hpp>
-#include "Physics.hpp"
-#include "Timer.hpp"
+#include "Application.hpp"
 
 #include <vector>
 #include <iostream>
-
 
 #define SIMULATION_WINDOW_WIDTH 800
 #define SIMULATION_WINDOW_HEIGHT 700
 
 #define TIME_STEP 1.f / 60.f
 
+class BallsApp : protected Application {  };
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);	
-void processInput(GLFWwindow *window);
-
-// settings
-const unsigned int SCR_WIDTH = 1920;
-const unsigned int SCR_HEIGHT = 1080;
-
-bool debug;
-Timer timer;
-float frames = 0.f;
-float deltaTime = 0.f;
-
-PhysicsController* physics; 
-
-void showDebugWindow() {
-	ImGui::Begin("debug");
-	ImGui::Text("Frames: %d", (int)frames);
-	ImGui::Text("Objects: %d", physics->getNumObjects());
-	ImGui::Text("Time Delta: %f", deltaTime);
-	//ImGui::Checkbox("Custom deltaTime", &useCustomDT);
-	//ImGui::DragFloat("Custom deltaTime value", &customDeltaTime, 0.00001f, 0.0005, 0.013, "%.4f");
-	ImGui::End();
-}
-
-int main()
-{
-#ifdef DEBUG
-	debug = 1;
-#endif
-	physics = new PhysicsController(SIMULATION_WINDOW_WIDTH, SIMULATION_WINDOW_HEIGHT);
-
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-
-	// glfw window creation
-	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	//init glew after the context have been made
-	glewInit();
-
-
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	// Setup Dear ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
-
-	// Setup Platform/Renderer backends
-	ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
-	ImGui_ImplOpenGL3_Init();
-
-	// render loop
-	// -----------
-	timer.start();
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-		// input
-		// -----
-		processInput(window);
-
-		// Start the Dear ImGui frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		physics->displaySimulation();
-		if (debug) showDebugWindow();
-
-		// render
-		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		// Rendering
-		// (Your code clears your framebuffer, renders your other stuff etc.)
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-		// Update and Render additional Platform Windows
-		ImGui::UpdatePlatformWindows();
-		ImGui::RenderPlatformWindowsDefault();
-	
-		
-		deltaTime = timer.readmarkSplitMillis() / 1000;
-		frames = 1.f / deltaTime;
-		physics->update(deltaTime);
-
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
-		glfwSwapBuffers(window);
-	}
-
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
-	glfwTerminate();
-
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+int main() {
+    BallsApp* app = new BallsApp();
 	return 0;
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	// make sure the viewport matches the new window dimensions; note that width and 
-	// height will be significantly larger than specified on retina displays.
-	glViewport(0, 0, width, height);
 }
--- premake5.lua
+++ premake5.lua
@@ -2,11 +2,13 @@
 
 
 workspace "Atomos" -- Name of sln file
-    location "project" -- Folder where to put it
     configurations { "Debug", "Release" }
     platforms { "Win32", "x64" }
+    toolset "msc"
 
-    cppdialect "C++latest"
+    outputdir = ("bin/%{cfg.buildcfg}/%{cfg.architecture}")
+    
+    cppdialect "C++20"
 
     --Set architecture
     filter { "platforms:Win32" }
@@ -16,63 +18,18 @@ workspace "Atomos" -- Name of sln file
     filter { "platforms:x64" }
         system "Windows"
         architecture "x64"
-
     filter { }
 
-project "Balls" -- Name of project
-    kind "ConsoleApp" -- Uses the console
-    language "C++"
-    location "project/Balls" -- location of vcxproj file
-    targetdir "bin/%{cfg.buildcfg}/%{cfg.architecture}" -- .exe files is in bin/(debug or release)/(x86 or x64)/
-    --location of source files to include. Here we include All files ending with .h and .cpp
-    --in the folder Minimal Example even files in subfolders.
-    files { 
-	    "./src/Balls/**.hpp", 
-	    "./src/Balls/**.cpp",
-	    "./lib/ImGui/*.cpp",
-	    "./lib/ImGui/*.h",
-	    "./lib/ImGui/backends/imgui_impl_glfw.*",
-	    "./lib/ImGui/backends/imgui_impl_opengl3.*",
-	    "premake5.lua"
-    } 
-
-    --Include directories
-    includedirs {
-        "./dependencies/glfw-3.2.1/include",
-        "./dependencies/glew-2.0.0/include",
-	    "./lib/ImGui",
-	    "./lib/ImGui/backends",
-        "./lib/glm"
-    }
-
-    --libraries and links
-    --links (the library files)
-    links {
-        "glew32",
-        "opengl32",
-        "glfw3"
-    }
-    --for 32 bit use these library paths
-    filter "architecture:x86"
-        libdirs { 
-            "./dependencies/glfw-3.2.1/win32/lib",
-            "./dependencies/glew-2.0.0/win32/lib"
-        }
+    filter { "system:windows", "action:gmake2" }
+        buildoptions { "--std=c++2a" }
     filter { }
-    --for x64 use these
-    filter "architecture:x64"
-        libdirs { 
-            "./dependencies/glfw-3.2.1/x64/lib",
-            "./dependencies/glew-2.0.0/x64/lib"
-        }
-    
-    --Debug and Release configurations
-    filter "configurations:Debug"
-        defines { "DEBUG" }
-        symbols "On"
 
-    filter "configurations:Release"
-        defines { "NDEBUG" }
-        optimize "On"
+    group "Dependencies"
+        include "Atomos/lib/Imgui/Build_ImGui.lua"
 
-    filter { }
+    group "Core"
+        include "Atomos/Build_Atomos.lua"
+
+
+    group "Demo"
+        include "Balls/Build_Balls.lua"
